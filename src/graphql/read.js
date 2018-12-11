95e1d9abdebf1cff7764a324957c7e5cee542964
Umran Hussein <umran.hussein@gmail.com>
2018-12-11 11:15:18 -0800

    test 2

--- src/graphql/read.js
+++ src/graphql/read.js
@@ -14,6 +14,7 @@ const prepare = responses => {
             return item.toString()
           })
         } else {
+          console.log('converting to string', key)
           response[key] = response[key].toString()
         }
       }
