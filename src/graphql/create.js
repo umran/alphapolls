4f41a3baae504004a7be6203d8a55a676f8c6fd1
Umran Hussein <umran.hussein@gmail.com>
2018-12-14 08:13:22 -0800

    more validations and bug fixes

--- src/graphql/create.js
+++ src/graphql/create.js
@@ -22,19 +22,55 @@ const prepare = response => {
 Object.keys(create).forEach(method => {
   exports[method] = async (args, context) => {
 
-    // if it is a response, check if the user has already participated in the survey_instance
     if (method === 'response') {
-      let survey_instance = await read.survey_instance({ _id: args.survey_instance })
+      //check if the user has already participated in the survey_instance
+      let survey_instance = await read.survey_instance({ primary: { _id: args.survey_instance } })
 
-      if (!survey_instance) {
-        throw new Error('the survey_instance does not exist')
+      if (!survey_instance || survey_instance.length === 0) {
+        throw new Error('the specified survey instance does not exist')
       }
 
-      if (survey_instance.user) {
-        if (survey_instance.user.indexOf(context.user)) {
+      if (survey_instance[0].user) {
+        if (survey_instance[0].user.indexOf(context.user)) {
           throw new Error('the user has already submitted a response to this survey instance')
         }
       }
+
+      // make sure no answer is associated with another response
+      let answers = await read.answer({ primary: { _id: $in: { args.answer } } })
+      if (!answers || answers.length === 0) {
+        throw new Error('none of the answers specified exist')
+      }
+
+      let addressedQuestions = []
+      for (var i=0; i<answers.length; i++) {
+        if (answers[i].response) {
+          throw new Error('at least one of the answers specified is already associated with another response')
+        }
+
+        addressedQuestions.push(answers[i].question)
+      }
+
+      addressedQuestions.sort()
+
+      // make sure answers address the right questions
+      let survey = await read.survey({ primary: { _id: survey_instance.survey } })
+      if (!survey || survey.length === 0) {
+        throw new Error('the specified survey does not exist')
+      }
+
+      let surveyQuestions = survey[0].question.map(question => question.toString()).sort()
+
+      if (addressedQuestions.length !== surveyQuestions.length) {
+        throw new Error('the addressed questions are not equivalent to the survey questions')
+      }
+
+      for (var i=0; i<addressedQuestions.length; i++) {
+        if (addressedQuestions[i] !== surveyQuestions[i]) {
+          throw new Error('at least one of the addressedQuestions does not match its counterpart survey question')
+        }
+      }
+
     }
 
     if (method === 'question' && args.input_type === 'Number') {
@@ -46,11 +82,11 @@ Object.keys(create).forEach(method => {
     if (method === 'answer') {
       let question = await read.question({ primary: { _id: args.question } })
 
-      if (!question) {
+      if (!question || question.length === 0) {
         throw new Error('the question does not exist')
       }
 
-      if (question.input_type === 'Number') {
+      if (question[0].input_type === 'Number') {
         args.value = Number(args.value)
       }
     }
